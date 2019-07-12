73f211a974e81ccbbcf9661764e122afaee3dedc
print-eye <52732051+print-eye@users.noreply.github.com>
2019-07-12 06:49:14 -0500

    Add files via upload

---
+++ 变量.py
@@ -0,0 +1,8 @@
+#变量
+name = "王二狗"
+age = "40"
+print("以前有个人，他叫"+name+"")
+print("他"+age+"岁了")
+name = "王大锤"
+print("他非常喜欢"+name+"这个名字")
+print("但是他不喜欢"+age+"岁")
---
+++ 字符串.py
@@ -0,0 +1,7 @@
+# 字符串 string str
+# slash(/) backslash(\)
+# 转义字符 escape character
+# 字符串拼接 concat
+mystr = "binary academy"
+# 尝试输出"binary" academy 单引号套双引号或转义字符\"
+print("bin" in mystr)
